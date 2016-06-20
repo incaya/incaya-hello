cf05e22b239108cddc049d74ec9753f2004a00b6
Thomas Maziere <thomas.maziere@gmail.com>
2016-06-20 12:20:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# incaya-hello
+An incredible hello react component
+
+```javascript
+import IncayaHello from 'IncayaHello';
+<IncayaHello />
+```
---
+++ incayahello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class IncayaHello extends React.Component {
+  render() {
+    return (
+      <div>Hello Incaya!</div>
+    )
+  }
+}
+
+export default IncayaHello;
